11756a8af35204713e4e6ba3ac652a897a03f8b7
AndreTharssysMarquesSoares <andre.tharssys.marques.soares@ccc.ufcg.edu.br>
2026-09-15 23:25:17 -0300

    fix: CORS

--- src/main/java/com/oficinapro/config/SecurityConfig.java
+++ src/main/java/com/oficinapro/config/SecurityConfig.java
@@ -102,7 +102,6 @@ public class SecurityConfig {
     configuration.setAllowedOrigins(allowedOrigins);
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
-    configuration.setAllowCredentials(true);
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
